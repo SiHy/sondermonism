01d81a7dbf22d8a419ff72c4c103b618dd1f8211
Simon Hymers <simonhymers@yahoo.co.uk>
2024-06-13 18:27:57 +0100

    Got up to date with current basic website as of 13-06-24

---
+++ public_html/dev/_unixclock.php
@@ -0,0 +1,15 @@
+<!-- sondermonism.com/ -->
+<!-- dev/_unixclock -->
+<!-- no frills unix time return -->
+
+<?php
+    /*
+    1 minute = 60
+    1 hour = 3600
+    1 day = 86400
+    1 week = 604800
+    1 month(30.44 days) = 2629743
+    1 year(365.24 days) = 31556926
+    */
+    echo sprintf('%.2f', round(microtime(true), 2))
+    ?>
---
+++ public_html/dev/unixclock.php
@@ -0,0 +1,32 @@
+<!-- sondermonism.com/ -->
+<!-- dev/unixclock -->
+
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+        <meta charset= "UTF-8">
+        <meta http-equiv="content-language" content="en-gb">
+        <meta name= "viewport" content= "width= device-width, initial-scale= 1.0">
+        <meta name= "description" content= "seconds passed since the start of January 1st 1970">
+        <meta name= "keywords" content= "unix time, UTC, seconds passed">
+        <meta name= "author" content= "SiHy">
+        <link rel= "stylesheet" href= "/public_html/standardstyle.css">
+        <title>
+            <?php echo "unixclock " . date_default_timezone_get() . " :: dev :: sonder \u{0298} monism" ?>
+        </title>
+    </head>
+    <body>
+        <?php
+        /*
+        1 minute = 60
+        1 hour = 3600
+        1 day = 86400
+        1 week = 604800
+        1 month(30.44 days) = 2629743
+        1 year(365.24 days) = 31556926
+        */
+        echo sprintf('%.2f', round(microtime(true), 2))
+        
+        ?>
+    </body>
+</html>
